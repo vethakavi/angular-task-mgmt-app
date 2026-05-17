b573b994d86aa1d83818a2e5eeb00b1623e4cf22
Kaviyarasan <vethakavi@gmail.com>
2026-05-17 20:44:37 +0530

    Environment file changed for cloud deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://nodejs-task-mgmt-app-be.onrender.com/api',
+  // apiUrl: 'https://nodejs-task-mgmt-app-be.onrender.com/api',
+  apiUrl: 'http://13.48.148.220:5000/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api',
+  apiUrl: 'http://13.48.148.220:5000/api', // http://localhost:5000/api
 };
